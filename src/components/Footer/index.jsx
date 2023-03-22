4b64bce267a8c47b2e23fae9a4f47591c78f9d39
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-22 11:44:48 -0300

    feat: Cria componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import Container from './styles';
+
+function Footer() {
+  return (
+    <Container>
+      Arlisson é muito lindo até no Footer
+    </Container>
+  );
+}
+
+export default Footer;
