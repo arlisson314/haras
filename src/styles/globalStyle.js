10030c356f42bb4ec85c1a0ad34b3d872cd6fba7
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-22 12:07:21 -0300

    style: Cria estilo Global

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Comfortaa', sans-serif;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  *, button, input {
+    border: 0;
+    font-family: 'Comfortaa', sans-serif;
+  }
+
+  :root {
+    --primary: #daac11ff;
+    --secundary: #7b4d0eff
+    --tertiary: #CFCFCF
+    --primary_font: #0e0e0eff
+    --secundary_font: #ffff
+  }
+`;
+// preto #0e0e0eff
+// amarelo #daac11ff
+// ocre #7b4d0eff
+// branco #ffff
+// cinza #CFCFCF
