3bbd5bc41b19aaf9ddabf27378dafa69ae352e71
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-22 11:46:58 -0300

    feat: Cria componente Home e adiciona os componentes Header e Footer

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+
+import Container from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <Header />
+      <h1>Arlisson é muito lindo!</h1>
+      <Footer />
+    </Container>
+  );
+}
+
+export default Home;
