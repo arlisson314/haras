d3f82a890b95757d6adc270ada9d759976809c89
lbseven7 <leobarbosapinturas@gmail.com>
2023-03-23 20:32:11 -0300

    style: Adciona estilo ao componente Footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,16 +4,16 @@ import img from '../../assets/footer-yahweh.png';
 export const Container = styled.footer`
   display: flex;
   text-align: center;
+  flex-direction: column;
   padding: 10px 0;
   width: 100%;
   height: 100px;
   background-image: url(${img});
 
-  p {
-    color: #f1f2f3;
+  >span {
+    margin-top: 10px;
+    color: var(--tertiary);
     font-size: 15px;
-    font-weight: 500;
-    margin: 20px 0;
   }
 
   div {
@@ -21,7 +21,17 @@ export const Container = styled.footer`
     justify-content: center;
     align-items: center;
     width: 100%;
-    gap: 20px;
+    gap: 10px;
+
+    >a {
+      margin-top: 10px;
+      color: var(--font);
+      font-size: 35px;
+    }
+
+    >a:hover {
+      color: var(--primary);
+    }
   }
 `;
 
