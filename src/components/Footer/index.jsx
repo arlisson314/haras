2f636d46304ba065222d4ca046aac0145769d779
lbseven7 <leobarbosapinturas@gmail.com>
2023-03-23 19:54:20 -0300

    style: Adiciona Link para icones no footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,23 +1,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaInstagram, FaYoutube } from 'react-icons/fa';
 import { Container } from './styles';
 
 const iconeProps = {
-  size: 30,
-  color: '#fff',
+  size: 40,
+  color: '#e3a403',
 };
 
 function Footer() {
   return (
     <Container>
+      <div>
+        <Link to="/">
+          <FaInstagram {...iconeProps} />
+        </Link>
+        <Link to="/">
+          <FaYoutube {...iconeProps} />
+        </Link>
+      </div>
       <p>Copyright © 2021. All rights reserved.</p>
-      <span>
-        <FaInstagram {...iconeProps} />
-      </span>
-      <span>
-        <FaYoutube {...iconeProps} />
-      </span>
     </Container>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -5,20 +5,23 @@ export const Container = styled.footer`
   display: flex;
   text-align: center;
   padding: 10px 0;
-  border: 2px solid red;
   width: 100%;
   height: 100px;
   background-image: url(${img});
 
   p {
-    color: #888;
-    border: 2px solid blue;
+    color: #f1f2f3;
+    font-size: 15px;
+    font-weight: 500;
+    margin: 20px 0;
   }
 
-  a {
-    color: #888;
-    border: 2px solid green;
-    text-decoration: none;
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    gap: 20px;
   }
 `;
 
