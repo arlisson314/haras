794c2ca732d3ddf2277153c8691e447b7b342ff1
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-24 15:52:44 -0300

    feat : Cria a rota galeria

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Galeria from './pages/Galeria';
 import Home from './pages/Home';
 
 function Router() {
@@ -7,6 +8,7 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/galeria" element={<Galeria />} />
       </Routes>
     </BrowserRouter>
   );
