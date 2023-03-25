6991a65825cdc08d4a03121f626df8c39a27c466
lbseven7 <leobarbosapinturas@gmail.com>
2023-03-25 20:24:52 -0300

    feat: Adiciona section no componente Cards com icones e texto

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,3 +1,5 @@
+import { FaVenusMars, FaHorseHead } from 'react-icons/fa';
+import { IoMdCalendar } from 'react-icons/io';
 import Container from './styles';
 import img from '../../assets/img_horse002.jpg';
 
@@ -5,10 +7,28 @@ function Cards() {
   return (
     <Container>
       <img src={img} alt="horse" />
+      <section>
+        <span>
+          <FaVenusMars />
+          Ops
+        </span>
+        <span>
+          <FaHorseHead />
+          Ops
+        </span>
+        <span>
+          <IoMdCalendar />
+          Ops
+        </span>
+      </section>
+
       <div>
         <h2>Cavalito Marchador</h2>
         <span>Genealogia: Cavalão Papai</span>
-        <span>Nascimento:12/02/2021</span>
+        <span>
+          <IoMdCalendar />
+          12/02/2021
+        </span>
       </div>
       <button type="button">Informações</button>
     </Container>
@@ -16,3 +36,4 @@ function Cards() {
 }
 
 export default Cards;
+// FaVenusMars
