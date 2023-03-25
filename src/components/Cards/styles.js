88308a0158a899861493921596d96c708c12eb1a
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-25 07:25:26 -0300

    style: adiciona estilo ao card

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -1,33 +1,33 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  display: flex;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  background-color: #f5f5f5;
   flex-direction: column;
   align-items: center;
-  width: 280px;
-  height: 390px;
-  background-color: #f5f5f5;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  font-size: 1rem;
+  font-size: 0.7rem;
+  display: flex;
+  width: 220px;
+  height: 300px;
   >img {
     object-fit: contain;
     width: 98%;
   }
 
   > div {
-    display: flex;
-    flex-direction: column;
     justify-content: flex-start;
+    flex-direction: column;
+    display: flex;
+    padding: 10px;
     width: 100%;
     height: 70%;
-    padding: 10px;
   }
 
   > button {
-    width: 80%;
-    height: 15%;
     background-color: var(--primary);
     margin-bottom: 15px;
+    height: 15%;
+    width: 80%;
   }
 `;
 
