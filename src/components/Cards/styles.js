a2fce6bcdf0806baa57c9ed746514ca5178eb505
lbseven7 <leobarbosapinturas@gmail.com>
2023-03-26 11:36:32 -0300

    Refactor: Refatora estilização do Container do componente Cards

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  background-color: #f5f5f5;
+  background-color: var(--bgCard);
   flex-direction: column;
   align-items: center;
   border-radius: 4px;
-  font-size: 0.7rem;
+  /* font-size: 0.7rem; */
   display: flex;
   height: 380px;
-  width: 270px;
+  width: 280px;
   >img {
     object-fit: contain;
     width: 100%;
@@ -44,14 +44,44 @@ const Container = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-around;
+    justify-content: center;
     width: 100%;
     min-height: 15%;
-    background: gray;
-    gap: 0.5em;
-    >span sp1{
-      background-color: white;
-      
+    background: white;
+  }
+  
+  >section {
+    display: flex;
+    
+    >div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid var(--tertiary);
+      width: 33.3%;
+      height: 100%;
+
+      >p {
+        font-size: 10px;
+        color: var(--secundary);
+        >span {
+          color: var(--infoCards);
+          margin: 1px;
+        }
+      }
+    }
+  }
+
+  >div {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    >h2 {
+      color: var(--infoCards);
+      font-size: 10px;
+    }
+    >span {
+      color: var(--infoCards);
     }
   }
 `;
