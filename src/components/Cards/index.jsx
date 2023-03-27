760e278e58013a1f93d7a3df2a266cea7f130186
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-27 16:06:22 -0300

    feat: Cria calculo de idade dos cavalos do card

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -3,7 +3,18 @@ import { IoMdCalendar } from 'react-icons/io';
 import Container from './styles';
 import img from '../../assets/img_horse002.jpg';
 
-function Cards() {
+function Cards({
+  name, sex, birthdate, father, mother, type,
+}) {
+  const hoje = new Date();
+  const birthDate = new Date(birthdate);
+  const diferencaEmMilissegundos = hoje - birthDate;
+  const milissegundosDia = 1000 * 60 * 60 * 24;
+  const milissegundosEmUmAno = 1000 * 60 * 60 * 24 * 365;
+
+  const anos = Math.floor(diferencaEmMilissegundos / milissegundosEmUmAno);
+  const meses = Math
+    .floor((diferencaEmMilissegundos % (milissegundosDia * 365)) / (milissegundosDia * 30));
   return (
     <Container>
       <img src={img} alt="horse" />
@@ -11,30 +22,49 @@ function Cards() {
         <div>
           <p>
             <FaVenusMars />
-            <span>Sexo</span>
+            <span>{sex}</span>
           </p>
         </div>
 
         <div>
           <p>
             <FaHorseHead />
-            <span>Tipo</span>
+            <span>{type}</span>
           </p>
         </div>
 
         <div>
           <p>
             <IoMdCalendar />
-            <span>Nasc</span>
+            <span>{birthdate}</span>
           </p>
         </div>
 
       </section>
 
       <div>
-        <span><strong>NOME DO ANIMAL</strong></span>
-        <span>(x) anos e (x) meses</span>
-        <span>(pai) xxx (mãe) xxx</span>
+        <h2>{name}</h2>
+        <span>
+          Idade:
+          {' '}
+          {String(anos).padStart(2, '0')}
+          {' '}
+          anos e
+          {' '}
+          {String(meses).padStart(2, '0')}
+          {' '}
+          meses
+        </span>
+        <span>
+          Pai:
+          {' '}
+          {father}
+        </span>
+        <span>
+          Mãe:
+          {' '}
+          {mother}
+        </span>
       </div>
 
       <button type="button">INFORMAÇÕES</button>
