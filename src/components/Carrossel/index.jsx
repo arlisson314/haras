9d3ded1121d56144685d2533813e5efe8e65da8b
lbseven7 <leobarbosapinturas@gmail.com>
2023-03-28 19:07:42 -0300

    feat: Cria funçao getParents para o carrossel

--- src/components/Carrossel/index.jsx
+++ src/components/Carrossel/index.jsx
@@ -1,13 +1,36 @@
-import img from '../../assets/img-sem-fundo.png';
+/* eslint-disable global-require */
+/* eslint-disable import/no-unresolved */
+import { Swiper, SwiperSlide } from 'swiper/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Container } from './styles';
+import 'swiper/css';
+// import img from '../../assets/horse.jpg';
 
 function Carrossel() {
+  const [parents, setParents] = useState([]);
+  const URL = 'http://localhost:3004/parents';
+
+  function getParents() {
+    axios.get(URL)
+      .then((response) => setParents(response.data))
+      .catch((err) => console.error(err));
+  }
+
+  useEffect(() => {
+    getParents();
+  }, []);
+
   return (
     <Container>
-      <h1>Haras YahWeh</h1>
-      <img src={img} alt="marchador" />
+      <Swiper>
+        {parents && parents.map((item) => (
+          <SwiperSlide key={item.id}>
+            <img src={require(item.image)} alt={item.name} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Container>
-
   );
 }
 
